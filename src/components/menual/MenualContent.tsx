9db2f793029b442041cd5575623b8a041c6609d7
jdy0120 <jdy0120120@gmail.com>
2021-07-21 17:21:53 +0900

    menual

--- src/components/menual/MenualContent.tsx
+++ src/components/menual/MenualContent.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../style/color';
 import multiSearch1 from '../../img/multiSearch1.png';
+import dragNdrop2 from '../../img/dragNdrop2.png';
+import fixUserTeam from '../../img/fixUserTeam.png';
+import fixUserTeam2 from '../../img/fixUserTeam2.png';
 
 const ContentText = styled.p`
     color: ${colors.fontColor};
 `;
 
 const ContentImg = styled.img`
-    width: 100px;
+    width: 100%;
+    image-rendering: auto;
 `;
 
 const MenualContent = () => {
@@ -20,19 +24,22 @@ const MenualContent = () => {
             <ContentText>
                 {'멀티서치 가능합니다.'}
             </ContentText>
-            <ContentImg src={multiSearch1} alt="" />
+            <ContentImg src={multiSearch1} />
             <ContentText>
                 {'유저 카드를 드래그해서 1팀 또는 2팀으로 이동할 수 있습니다.'}
             </ContentText>
+            <ContentImg src={dragNdrop2} />
             <ContentText>
                 {'꼭 같은 팀이어야 하는 유저들을 1팀 또는 2팀으로 이동시켜 팀을 나눌 수 있습니다.'}
             </ContentText>
+            <ContentImg src={fixUserTeam} />
+            <ContentImg src={fixUserTeam2} />
             <ContentText>
                 {'자신있는 라인을 선택해 팀을 나눌 수 있습니다. 만약 같은 라인에 3명 이상의 유저가 지정되어있다면 MMR이 높은 유저 두명을 기준으로 팀이 나눠집니다.'}
             </ContentText>
-            <ContentText>
+            {/* <ContentText>
                 {'솔로 랭크 기준으로 MMR을 계산했으면 계산법은 아래 링크에서 확인하실 수 있습니다.'}
-            </ContentText>
+            </ContentText> */}
             <ContentText>
                 {'만약 언랭 유저인 경우 실버2 0포인트로 생각하여 MMR을 계산합니다.'}
             </ContentText>
