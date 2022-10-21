022d62937774f4bbe9b9132802ba9135add9ff15
jdy0120 <jdy0120120@gmail.com>
2022-10-21 16:24:11 +0900

    update reademe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import InsertUser from './components/InsertUser';
-import DivTeam from './components/DivTeam'
-import { UserInfo } from '../types/type'
-import styled from 'styled-components';
-import DragNDrop from './components/DragNDrop';
-import { colors } from './style/color';
-import Adsense from './components/Adsense';
-import Menual from './components/menual/Menual';
+import React from "react";
+import InsertUser from "./components/InsertUser";
+import DivTeam from "./components/DivTeam";
+import { UserInfo } from "../types/type";
+import styled from "styled-components";
+import DragNDrop from "./components/DragNDrop";
+import { colors } from "./style/color";
+import Adsense from "./components/Adsense";
+import Menual from "./components/menual/Menual";
 
 const SelectOptionUser = styled.div`
   text-align: center;
@@ -33,7 +33,7 @@ const DivTeamButton = styled.button`
 
   &:hover {
     background-color: ${colors.background};
-    color:  #BDBDC0;
+    color: #bdbdc0;
     cursor: pointer;
   }
 `;
@@ -44,40 +44,43 @@ function App() {
 
   const userLang = navigator.language;
 
-  console.log(userLang);
-
   const clickDivTeam = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (userInfoList.length%2 !== 0) {
-      alert('인원을 짝수로 맞춰주세요')
+    if (userInfoList.length % 2 !== 0) {
+      alert("인원을 짝수로 맞춰주세요");
     } else {
-      setRunDivTeam(true)
+      setRunDivTeam(true);
     }
-  }
+  };
 
   const clickSearchTeam = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setRunDivTeam(false)
-  }
-  
-  React.useEffect(() => {
-  },[userInfoList])
+    setRunDivTeam(false);
+  };
+
+  React.useEffect(() => {}, [userInfoList]);
 
   return (
     <SelectOptionUser>
-      {runDivTeam === false ?
+      {runDivTeam === false ? (
         <>
           <div>
-            <Menual/>
-            <InsertUser userInfoList={userInfoList} setUserInfoList={setUserInfoList}/>
+            <Menual />
+            <InsertUser
+              userInfoList={userInfoList}
+              setUserInfoList={setUserInfoList}
+            />
           </div>
-          <DragNDrop userInfoList={userInfoList} setUserInfoList={setUserInfoList}/>
-          <DivTeamButton onClick={clickDivTeam}>{'팀 나누기'}</DivTeamButton>
+          <DragNDrop
+            userInfoList={userInfoList}
+            setUserInfoList={setUserInfoList}
+          />
+          <DivTeamButton onClick={clickDivTeam}>{"팀 나누기"}</DivTeamButton>
         </>
-      :
-      <div>
-        <DivTeam userInfoList={userInfoList} />
-        <DivTeamButton onClick={clickSearchTeam}>{'인원 수정'}</DivTeamButton>
-      </div>
-      }
+      ) : (
+        <div>
+          <DivTeam userInfoList={userInfoList} />
+          <DivTeamButton onClick={clickSearchTeam}>{"인원 수정"}</DivTeamButton>
+        </div>
+      )}
       <Adsense />
     </SelectOptionUser>
   );
--- src/utils/getUserInfo.ts
+++ src/utils/getUserInfo.ts
@@ -1,22 +1,26 @@
-import axios from 'axios'
-import { serverUrl } from '../assets/config'
+import axios from "axios";
+import { serverUrl } from "../assets/config";
 
-export const getUserAccountId = async (userId:string): Promise<any>=> {
+export const getUserAccountId = async (userId: string): Promise<any> => {
     try {
         // const response = await axios.get(`http://localhost:5000/seismic-sweep-318403/us-central1/getUserInfo/fetchUserAccountId?userId=${userId}`);
-        const response = await axios.get(`${serverUrl}/getUserInfo/fetchUserAccountId?userId=${userId}`);
-        return response.data.userAccountId
+        const response = await axios.get(
+            `${serverUrl}/getUserInfo/fetchUserAccountId?userId=${userId}`
+        );
+        return response.data.userAccountId;
     } catch (err) {
         throw new Error(err.response.data);
     }
-}
+};
 
-export const getUserInfo = async (id:string): Promise<any> => {
+export const getUserInfo = async (id: string): Promise<any> => {
     try {
         // const response = await axios.get(`http://localhost:5000/seismic-sweep-318403/us-central1/getUserInfo/fetchUserInfo?id=${id}`,)
-        const response = await axios.get(`${serverUrl}/getUserInfo/fetchUserInfo?id=${id}`,)
-        return response.data.userInfo
+        const response = await axios.get(
+            `${serverUrl}/getUserInfo/fetchUserInfo?id=${id}`
+        );
+        return response.data.userInfo;
     } catch (err) {
         throw new Error(err.response.data);
     }
-}
+};
